f8694e81ce0236a62322780c67bf79570615fb5a
fengnbiao <98457893+FengniaoCode@users.noreply.github.com>
2022-02-14 20:45:53 +0800

    561

--- app02/models.py
+++ app02/models.py
@@ -15,3 +15,25 @@ class User(models.Model):
     age = models.IntegerField(verbose_name="年龄")
     account = models.DecimalField(verbose_name="账户余额", max_digits=10, decimal_places=2)
     create_date = models.DateTimeField(verbose_name="入职时间")
+
+    # depar_id = models.BigIntegerField(verbose_name="部门id")   ->这样无约束，ID可能随意的写
+
+    # 有约束
+    #  — ForeignKey： 表示设置外键
+    #  - to：与哪张表关联
+    #  - to_fields： 与表中的哪一列关联，即外键关联的主键
+    # 注意：
+    # Django自动将写的depart生成depart_id，即数据库表中列名是depart_id，不是depart
+    #   级联删除：
+    # depart = models.ForeignKey(to="Department", to_fields="id",on_delete=models.CASCADE)
+    #   置空：会把外键设置为null，前提是depart列允许为null（下面代码：null=True, blank=True）。
+    depart = models.ForeignKey(to="Department", to_field="id", null=True, blank=True, on_delete=models.SET_NULL)
+
+    # 性别：
+    # 在Django中做的约束
+    gender_choices = (
+        (1, "男"),
+        (2, "女"),
+
+    )
+    gender = models.SmallIntegerField(verbose_name="性别", choices=gender_choices)
--- programGit/settings.py
+++ programGit/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'programGit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'management',  # 数据库名字
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',  # 那台机器安装了MySQL
+        'PORT': 3306,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
